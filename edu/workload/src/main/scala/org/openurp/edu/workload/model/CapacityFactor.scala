082b06d1ac7ffc96934317358ecd565468a1e4c8
段体华 <duantihua@gmail.com>
2018-11-05 23:47:13 +0800

    fix #20 增加了工作量相关结构

---
+++ edu/workload/src/main/scala/org/openurp/edu/workload/model/CapacityFactor.scala
@@ -0,0 +1,44 @@
+/*
+ * OpenURP, Agile University Resource Planning Solution.
+ *
+ * Copyright (c) 2005, The OpenURP Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openurp.edu.workload.model
+
+import org.beangle.commons.collection.Collections
+import org.beangle.data.model.LongId
+import org.beangle.data.model.pojo.Named
+import org.beangle.data.model.pojo.Remark
+import org.beangle.data.model.pojo.TemporalOn
+
+class CapacityFactor extends LongId with Named with Remark with TemporalOn {
+
+  var condition: String = _
+
+  var segments = Collections.newBuffer[FactorSegment]
+
+}
+
+class FactorSegment extends LongId {
+
+  var factor: CapacityFactor = _
+
+  var minCapacity: Int = _
+
+  var maxCapacity: Int = _
+
+  var factorExp: String = _
+}
