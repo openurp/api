1b5b05e6b10f2d67fc4b552be85382117d37a794
段体华 <duantihua@gmail.com>
2020-09-06 23:26:45 +0800

    fix #73 增加教室借用模型

---
+++ edu/room/src/main/scala/org/openurp/edu/room/model/RoomApply.scala
@@ -0,0 +1,142 @@
+/*
+ * OpenURP, Agile University Resource Planning Solution.
+ *
+ * Copyright © 2014, The OpenURP Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful.
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openurp.edu.room.model
+
+import java.time.Instant
+
+import org.beangle.commons.collection.Collections
+import org.beangle.commons.lang.time.WeekTime
+import org.beangle.data.model.pojo.{DateRange, Named}
+import org.beangle.data.model.{Component, LongId}
+import org.openurp.base.model.{Campus, Department, School, User}
+import org.openurp.code.edu.model.ActivityType
+
+import scala.collection.mutable
+
+class RoomApply extends LongId {
+
+  /** 学校 */
+  var school: School = _
+
+  /** 借用人 */
+  var borrower: Borrower = _
+
+  /** 时间要求 */
+  var time: TimeRequest = _
+
+  /** 教室要求 */
+  var space: SpaceRequest = _
+
+  /** 活动 */
+  var activity: Activity = _
+
+  /** 申请时间 */
+  var applyAt: Instant = _
+
+  /** 操作人 */
+  var applyBy: User = _
+
+  /** 部门审核 */
+  var departCheck: Option[ApplyDepartCheck] = None
+
+  /** 最终审核 */
+  var finalCheck: Option[ApplyFinalCheck] = None
+
+  /** 是否通过 */
+  var approved: Option[Boolean] = None
+}
+
+class Activity extends Component with Named {
+  /** 活动类型 */
+  var activityType: ActivityType = _
+
+  /** 主讲人：姓名及背景资料 */
+  var speaker: String = _
+
+  /** 出席对象 */
+  var attendance: String = _
+
+  /** 出席人数 */
+  var attendanceNum: Int = _
+}
+
+/** 借用人 */
+class Borrower extends Cloneable with Component {
+
+  /** 借用人部门 */
+  var department: Department = _
+
+  /** 借用人 */
+  var applicant: String = _
+
+  /** 移动电话 */
+  var mobile: String = _
+
+  /** 电子邮件 */
+  var email: Option[String] = None
+}
+
+/** 时间要求 */
+class TimeRequest extends Component with DateRange {
+  /** 分钟 */
+  var minutes: Int = 0
+
+  /** 借用时间要求 */
+  var timeComment: Option[String] = None
+
+  /** 申请时间 */
+  var times: mutable.Buffer[WeekTime] = Collections.newBuffer[WeekTime]
+}
+
+/** 教室要求 */
+class SpaceRequest extends Component {
+  /** 借用校区 */
+  var campus: Campus = _
+
+  /** 每个教室单元需要的座位数 */
+  var unitAttendance: Int = 0
+
+  /** 是否使用多媒体设备 */
+  var requireMultimedia: Boolean = false
+
+  /** 借用场所要求 */
+  var roomComment: Option[String] = None
+
+}
+
+class ApplyDepartCheck extends LongId {
+  var apply: RoomApply = _
+  /**是否审核通过*/
+  var approved: Boolean = _
+  /**审核人*/
+  var checkedBy: User = _
+  /**审核时间*/
+  var checkedAt: Instant = _
+  /**具体意见*/
+  var opinions: Option[String] = None
+}
+
+class ApplyFinalCheck extends LongId {
+  var apply: RoomApply = _
+  var approved: Boolean = _
+  var checkedBy: User = _
+  var checkedAt: Instant = _
+  var opinions: Option[String] = None
+}
+
