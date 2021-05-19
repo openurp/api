196358db9e74e6f91480e1be749d4131b8fb8402
段体华 <duantihua@gmail.com>
2021-05-19 22:48:20 +0800

    update 0.23.0.sql

--- all/src/main/resources/db/postgresql/migrate/0.23.0.sql
+++ all/src/main/resources/db/postgresql/migrate/0.23.0.sql
@@ -2,4 +2,6 @@ insert into base.versions(id,version,updated_at,description)
 values(next_id('base.versions'),'0.23.0',now(),'更名考务教室分组和分配表');
 
 alter table edu.room_groups rename to exam_room_groups;
+alter table edu.room_groups_rooms rename to exam_room_groups_rooms;
+alter table edu.exam_room_groups_rooms rename column room_group_id to exam_room_group_id;
 alter table edu.room_alloc_settings to exam_alloc_settings;
