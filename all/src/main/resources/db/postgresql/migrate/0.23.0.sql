a54f7c2d1c85e4812093bff6a6b9f1178d8abcd2
段体华 <duantihua@gmail.com>
2021-05-20 00:01:54 +0800

    bump version 0.23.1-SNAPSHOT

--- all/src/main/resources/db/postgresql/migrate/0.23.0.sql
+++ all/src/main/resources/db/postgresql/migrate/0.23.0.sql
@@ -4,4 +4,4 @@ values(next_id('base.versions'),'0.23.0',now(),'更名考务教室分组和分�
 alter table edu.room_groups rename to exam_room_groups;
 alter table edu.room_groups_rooms rename to exam_room_groups_rooms;
 alter table edu.exam_room_groups_rooms rename column room_group_id to exam_room_group_id;
-alter table edu.room_alloc_settings to exam_alloc_settings;
+alter table edu.room_alloc_settings rename to exam_alloc_settings;
